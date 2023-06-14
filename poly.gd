24c583e996cf3f2fef4e5448a723ab35da0591d7
Mike Kinney <mike.kinney@gmail.com>
2023-06-14 09:20:03 -0700

    try to use signals

--- poly.gd
+++ poly.gd
@@ -1,2 +1,18 @@
 extends Polygon2D
 class_name Poly
+
+signal poly_entered
+
+func _on_Poly_mouse_entered():
+	print("poly entered")
+	emit_signal("poly_entered")
+
+func _on_mouse_entered():
+	print("mouse entered")
+	emit_signal("poly_entered")
+
+func _on_focus_entered():
+	print("focus entered")
+	
+func _on_Poly_focus_entered():
+	print("poly focus entered")	
--- polys.gd
+++ polys.gd
@@ -9,6 +9,7 @@ func _ready():
 	poly1.set_polygon(Array([Vector2(40, 40), Vector2(40, 50), Vector2(50, 50), Vector2(50, 40)]))
 	var coll1 = CollisionPolygon2D.new()
 	coll1.set_polygon(poly1.get_polygon())
+	poly1.poly_entered.connect(poly1_entered)
 	add_child(poly1)
 		
 	var poly2 = Poly.new()
@@ -16,7 +17,11 @@ func _ready():
 	poly2.set_color(Color.RED)
 	add_child(poly2)
 
+func poly1_entered():
+	print("woo hoo")
+
 	
 func _input(event):
 	if event.is_pressed():
 		print("hello4")	
+
